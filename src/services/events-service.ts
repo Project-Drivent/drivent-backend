3871a948a5d5be4ff65f010949bf66f0454322bd
João Vitor Diniz Mistrinel <97896243+JohnDiniz@users.noreply.github.com>
2023-10-18 15:07:57 -0300

    Add Redis support to the hotels function

--- src/services/events-service.ts
+++ src/services/events-service.ts
@@ -1,17 +1,25 @@
 import { Event } from '@prisma/client';
 import dayjs from 'dayjs';
+import redis from '../config/redis';
 import { notFoundError } from '@/errors';
 import { eventRepository } from '@/repositories';
 import { exclude } from '@/utils/prisma-utils';
 
+export type GetFirstEventResult = Omit<Event, 'createdAt' | 'updatedAt'>;
+
 async function getFirstEvent(): Promise<GetFirstEventResult> {
+  const cachedEvent = await redis.get('firstEvent');
+  if (cachedEvent) {
+    return JSON.parse(cachedEvent);
+  }
+
   const event = await eventRepository.findFirst();
   if (!event) throw notFoundError();
 
-  return exclude(event, 'createdAt', 'updatedAt');
+  const eventInfo = exclude(event, 'createdAt', 'updatedAt');
+  await redis.set('firstEvent', JSON.stringify(eventInfo));
+  return eventInfo;
 }
-
-export type GetFirstEventResult = Omit<Event, 'createdAt' | 'updatedAt'>;
 
 async function isCurrentEventActive(): Promise<boolean> {
   const event = await eventRepository.findFirst();
